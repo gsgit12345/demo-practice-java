0bf797a1e3a6ac19f9f01c06985382b7913c5023
ghanshyam shukla <gsgmca682@gmail.com>
2023-10-02 09:26:55 -0700

    code committed for sliding window-part3

---
+++ demo-corejava-demo/src/main/java/org/example/datastructure/linkedlist/KthNodeInLinkedList.java
@@ -0,0 +1,18 @@
+package org.example.datastructure.linkedlist;
+
+public class KthNodeInLinkedList {
+    public static void main(String str[])
+    {
+        LinkedList linkedList=new LinkedList();
+        linkedList.addAtFirst(40);
+
+        linkedList.addAtFirst(10);
+        linkedList.addAtFirst(0);
+        linkedList.addAtFirst(20);
+        linkedList.addAtFirst(30);
+        linkedList.addAtFirst(130);
+        linkedList.addAtFirst(230);
+
+        linkedList.displayLinkedList();
+    }
+}
